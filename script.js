6aa4f873b1e87b79a38774e4784186d17b2c64cf
seaboyz <seaboyz@hotmail.com>
2021-10-29 19:44:16 -0500

    Use get and set attribute instead of dataset

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 var nav = document.querySelector('.primary-navigation');
 var navToggle = document.querySelector('.mobile-nav-toggle');
 navToggle === null || navToggle === void 0 ? void 0 : navToggle.addEventListener('click', function () {
-    if ((nav === null || nav === void 0 ? void 0 : nav.dataset.visible) === 'true') {
-        nav.dataset.visible = 'false';
-        navToggle === null || navToggle === void 0 ? void 0 : navToggle.setAttribute('aria-expanded', 'false');
+    if ((nav === null || nav === void 0 ? void 0 : nav.getAttribute('data-visible')) === 'true') {
+        nav.setAttribute('data-visible', 'false');
+        navToggle.setAttribute('aria-expanded', 'false');
     }
     else {
-        nav.dataset.visible = 'true';
-        navToggle === null || navToggle === void 0 ? void 0 : navToggle.setAttribute('aria-expanded', 'true');
+        nav === null || nav === void 0 ? void 0 : nav.setAttribute('data-visible', 'true');
+        navToggle.setAttribute('aria-expanded', 'true');
     }
 });
--- script.ts
+++ script.ts
@@ -1,20 +1,14 @@
-const nav:
-	| ({
-			dataset: {
-				visible: 'true' | 'false';
-			};
-	  } & HTMLElement)
-	| null = document.querySelector('.primary-navigation');
+const nav: HTMLElement | null = document.querySelector('.primary-navigation');
 
 const navToggle: HTMLElement | null =
 	document.querySelector('.mobile-nav-toggle');
 
 navToggle?.addEventListener('click', () => {
-	if (nav?.dataset.visible === 'true') {
-		nav!.dataset.visible = 'false';
-		navToggle?.setAttribute('aria-expanded', 'false');
+	if (nav?.getAttribute('data-visible') === 'true') {
+		nav.setAttribute('data-visible', 'false');
+		navToggle.setAttribute('aria-expanded', 'false');
 	} else {
-		nav!.dataset.visible = 'true';
-		navToggle?.setAttribute('aria-expanded', 'true');
+		nav?.setAttribute('data-visible', 'true');
+		navToggle.setAttribute('aria-expanded', 'true');
 	}
 });
