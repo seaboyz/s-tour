f2c7a2fff2480b48ec535ed7e85e5f6c22029ed2
seaboyz <seaboyz@hotmail.com>
2021-12-16 13:48:25 -0600

    Add destination route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 
 const rootRoute = require('./routes/root')
+const destinationRoute = require('./routes/destination')
 
 const app = express()
 
 // root route
 app.use('/', rootRoute)
 
+// destintion route
+app.use('/destination', destinationRoute)
 
 app.listen(3000)
