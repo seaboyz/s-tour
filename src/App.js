58c93a21bb6937f06c9ea5e4bcc116e207cdfef6
seaboyz <seaboyz@hotmail.com>
2022-01-28 16:11:22 -0600

    Add Homepage and move assets to src folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import HomePage from './pages/home/home.page.jsx';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Home page</h1>
+			<HomePage />
 		</div>
 	);
 }
---
+++ src/pages/home/home.page.jsx
@@ -0,0 +1,9 @@
+import './home.styles.scss';
+
+const HomePage = () => (
+	<div>
+		<h1>Home Page</h1>
+	</div>
+);
+
+export default HomePage;
